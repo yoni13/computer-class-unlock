98fdedac24faec3142d128b80cf8bf74fdca73dc
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-12-14 18:25:37 +0800

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,27 @@
+taskkill /f /im RemoteControlServer.exe
+
+taskkill /f /im RemoteControlServer.exe
+
+taskkill /f /im PNSClient64.exe
+
+taskkill /f /im PNSClient.exe
+
+taskkill /f /im AnyTimeSrv.exe
+
+taskkill /f /im ProjectorService.exe
+
+taskkill /f /im Student.exe
+
+taskkill /f /im PNSApp64.exe
+
+taskkill /f /im PNSAppPrt.exe
+
+taskkill /f /im PNSMonitor.exe
+
+taskkill /f /im PnsResetSecret.exe
+
+taskkill /f /im RemoteDesk64.exe
+
+cd C:\Program Files (x86)
+
+echo y|rmdir /s TeamSoftEx
